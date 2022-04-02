c7f4f3a27885e401e615a65cbb100958757f9f5c
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-04-02 18:11:54 +0600

    Complete Project Added

--- app.py
+++ app.py
@@ -67,6 +67,20 @@ def edit(id):
         return render_template('edit.html', post=post)
 
 
+@app.route('/posts/new', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('new_post.html')
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
