5d8bf88c651ea24674b6ddb6372c00c23104fa2c
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-04-02 17:28:13 +0600

    Create, Update, Delete

--- app.py
+++ app.py
@@ -1,51 +1,71 @@
-from flask import Flask, render_template
-
+from email.policy import default
+from flask import Flask, render_template, request, jsonify, redirect
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 
-all_posts = [
 
-    {
-        'title': 'Post 1',
-        'content': 'This is the first post',
-        'author': 'John Doe',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'title': 'Post 2',
-        'content': 'This is the second post',
-        'author': 'Owali',
-        'date_posted': 'April 21, 2021'
-    },
-    {
-        'title': 'Post 3',
-        'content': 'This is the third post',
-        'author': 'Ruhul',
-        'date_posted': 'April 1, 2008'
-    },
-    {
-        'title': 'Post 4',
-        'content': 'This is the fourth post',
-        'date_posted': 'April 20, 2018'
-    }
-
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
 
 
-@app.route('/', methods=['GET'])
-@app.route('/<int:id>', methods=['GET']) 
-def index(id):
-    return 'Hello, ' + str(id) + ' !'
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
 
 
+    def __repr__(self):
+        return 'Blog post ' + str(self.id)
+
+
+
+
+@app.route('/')
 @app.route('/home', methods=['GET'])
 def home():
     return render_template('index.html')
 
 
-@app.route('/posts', methods=['GET'])
+@app.route('/posts', methods=['GET', 'POST'])
 def posts():
-    return render_template('posts.html', posts=all_posts)
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('posts.html', posts=all_posts)
+   
+
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    post = BlogPost.query.get_or_404(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
+
 
 
 if __name__ == '__main__':
